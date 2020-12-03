e997675a6318cc680ebdf30d8a62f547e099b0b3
Yoeri Houtzager <Yoeri.Houtzager@keylane.com>
2020-12-03 15:25:26 +0100

    day 3 neat

--- problems/day3/day3neat.py
+++ problems/day3/day3neat.py
@@ -2,7 +2,7 @@ import time
 
 
 def create_coordinates(slope_x, slope_y, width, height):
-    return [[slope_x * i % width, i] for i in range(0, height, slope_y)]
+    return [[slope_x * (i//slope_y) % width, i] for i in range(0, height, slope_y)]
 
 
 if __name__ == '__main__':
@@ -19,15 +19,17 @@ if __name__ == '__main__':
 
 
     print('Answer part 1: {}'.format(answer))
-    slopes = {[1, 1]: 0, [3, 1]: 0, [5, 1]: 0, [7, 1]: 0, [1, 2]: 0}
 
-    for slope in slopes.keys():
-        for coordinate in create_coordinates(slope[0], slope[1], len(array[0]), len(array)):
-            if array[coordinate[1]][coordinate[0]] == '#':
-                slopes[slope] += 1
+    slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
 
     answer2 = 1
-    answer2 = [answer2 * i for i in slopes.values()]
+    for slope in slopes:
+        trees = 0
+        for coordinate in create_coordinates(slope[0], slope[1], len(array[0]), len(array)):
+            if array[coordinate[1]][coordinate[0]] == '#':
+                trees += 1
+        answer2 *= trees
+
     print('Answer part 2: {}'.format(answer2))
 
     end = time.time()
